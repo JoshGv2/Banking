e7cdc6948e78a48518d0f87e0c7288a78f18288a
Nothando Tshuma <tahumazoxx@gmail.com>
2022-10-31 16:39:36 +0000

    added "_test" to test methods

--- src/test/java/org/example/BankAccountTest.java
+++ src/test/java/org/example/BankAccountTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class BankAccountTest {
 
     @Test
-    void createAccountTest(){
+    void test_createAccountTest(){
         BankAccount joshBankAccount = new BankAccount( 500,"Josh", BankAccount.AccType.SAVINGS);
         BankAccount nothandoBankAccount = new BankAccount( 5000,"Nothando", BankAccount.AccType.SAVINGS);
         assertEquals("Josh", joshBankAccount.getAccHolderName());
@@ -16,7 +16,7 @@ public class BankAccountTest {
     }
 
     @Test
-    void depositTest(){
+    void test_depositTest(){
         BankAccount joshBankAccount = new BankAccount( 500,"Josh", BankAccount.AccType.SAVINGS);
         joshBankAccount.deposit(1000);
         assertEquals(1500, joshBankAccount.getBalance());
@@ -24,14 +24,14 @@ public class BankAccountTest {
     }
 
     @Test
-    void withdrawTest(){
+    void test_withdrawTest(){
         BankAccount nothandoBankAccount = new BankAccount( 5000,"Nothando", BankAccount.AccType.SAVINGS);
         nothandoBankAccount.withdraw(1000);
         assertEquals(4000, nothandoBankAccount.getBalance());
     }
 
     @Test
-    void depositNegativeTest() throws Exception{
+    void test_depositNegativeTest() throws Exception{
         BankAccount joshBankAccount = new BankAccount( 500,"Josh", BankAccount.AccType.SAVINGS);
         joshBankAccount.deposit(1000);
         assertEquals(1500, joshBankAccount.getBalance());
@@ -39,7 +39,7 @@ public class BankAccountTest {
     }
 
     @Test
-    void createCurrentAccTest() throws Exception {
+    void test_createCurrentAccTest() throws Exception {
         BankAccount joshBankAccount = new CurrentAccount( 500,"Josh", BankAccount.AccType.SAVINGS, 250);
         joshBankAccount.deposit(1000);
         assertEquals(1500, joshBankAccount.getBalance());
@@ -47,13 +47,10 @@ public class BankAccountTest {
     }
 
     @Test
-    void createSavingsAccTest(){
+    void test_createSavingsAccTest(){
         BankAccount joshBankAccount = new SavingsAcc( 500,"Josh", BankAccount.AccType.SAVINGS, 250);
         joshBankAccount.deposit(1000);
         assertEquals(1500, joshBankAccount.getBalance());
         assertThrows(IllegalArgumentException.class, ()->joshBankAccount.deposit(-5000));
     }
 }
-
-
-
